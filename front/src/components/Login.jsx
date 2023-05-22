ee1b45201615166966e45a7c470497cbc9c73293
nemanja731 <nemanja.jankovic731@gmail.com>
2023-05-22 03:37:56 +0200

    Update login

--- front/src/components/Login.jsx
+++ front/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import {
   faCheck,
   faTimes,
@@ -21,6 +21,8 @@ function Login() {
   const [wrongPassword, setWrongPassword] = useState(false);
 
   const [success, setSuccess] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     setEmptyUsername(false);
@@ -63,6 +65,7 @@ function Login() {
           setWrongUsername(false);
           setWrongPassword(false);
           setSuccess(true);
+          navigate(username);
         } else {
           setWrongUsername(true);
           setWrongPassword(true);
@@ -72,100 +75,87 @@ function Login() {
   };
 
   return (
-    <>
-      {success ? (
-        <section>
-          <h1>Success!</h1>
-          <p>
-            You have successfully logged in!
-            <br />
-            <a href="#"></a>
-          </p>
-        </section>
-      ) : (
-        <div className="auth-form-container">
-          <h2>Login</h2>
-          <form className="login-form" onSubmit={handleSubmit}>
-            <label htmlFor="username">
-              Username:
-              <FontAwesomeIcon
-                icon={faCheck}
-                className={
-                  username !== "" && !emptyUsername && !wrongUsername
-                    ? "valid"
-                    : "hide"
-                }
-              />
-              <FontAwesomeIcon
-                icon={faTimes}
-                className={wrongUsername ? "invalid" : "hide"}
-              />
-            </label>
-            <input
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              type="username"
-              placeholder="username"
-              id="username"
-              name="username"
-            ></input>
-            <label htmlFor="password">
-              Password:
-              <FontAwesomeIcon
-                icon={faCheck}
-                className={
-                  password !== "" && !emptyPassword && !wrongPassword
-                    ? "valid"
-                    : "hide"
-                }
-              />
-              <FontAwesomeIcon
-                icon={faTimes}
-                className={wrongPassword ? "invalid" : "hide"}
-              />
-            </label>
-            <input
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              type="password"
-              placeholder="********"
-              id="password"
-              name="password"
-            ></input>
-            <button className="submit-btn" type="submit">
-              Log In
-            </button>
-            <p
-              id="confirmnote"
-              className={
-                wrongUsername && wrongPassword ? "instructions" : "offscreen"
-              }
-            >
-              <FontAwesomeIcon icon={faInfoCircle} />
-              Wrong username or password.
-            </p>
-            <p
-              id="confirmnote"
-              className={emptyUsername ? "instructions" : "offscreen"}
-            >
-              <FontAwesomeIcon icon={faInfoCircle} />
-              You didn't fill username.
-            </p>
-            <p
-              id="confirmnote"
-              className={emptyPassword ? "instructions" : "offscreen"}
-            >
-              <FontAwesomeIcon icon={faInfoCircle} />
-              You didn't fill password.
-            </p>
-          </form>
-          <p className="sign-paragraph">Don't have an account?</p>
-          <NavLink className="navlink" to="/register">
-            Sign Up
-          </NavLink>
-        </div>
-      )}
-    </>
+    <div className="auth-form-container">
+      <h2>Login</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label htmlFor="username">
+          Username:
+          <FontAwesomeIcon
+            icon={faCheck}
+            className={
+              username !== "" && !emptyUsername && !wrongUsername
+                ? "valid"
+                : "hide"
+            }
+          />
+          <FontAwesomeIcon
+            icon={faTimes}
+            className={wrongUsername ? "invalid" : "hide"}
+          />
+        </label>
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          type="username"
+          placeholder="username"
+          id="username"
+          name="username"
+        ></input>
+        <label htmlFor="password">
+          Password:
+          <FontAwesomeIcon
+            icon={faCheck}
+            className={
+              password !== "" && !emptyPassword && !wrongPassword
+                ? "valid"
+                : "hide"
+            }
+          />
+          <FontAwesomeIcon
+            icon={faTimes}
+            className={wrongPassword ? "invalid" : "hide"}
+          />
+        </label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+        ></input>
+        <button className="submit-btn" type="submit">
+          Log In
+        </button>
+        <p
+          id="confirmnote"
+          className={
+            wrongUsername && wrongPassword ? "instructions" : "offscreen"
+          }
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+          Wrong username or password.
+        </p>
+        <p
+          id="confirmnote"
+          className={emptyUsername ? "instructions" : "offscreen"}
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+          You didn't fill username.
+        </p>
+        <p
+          id="confirmnote"
+          className={emptyPassword ? "instructions" : "offscreen"}
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+          You didn't fill password.
+        </p>
+      </form>
+      <p className="sign-paragraph">Don't have an account?</p>
+      <NavLink className="navlink" to="/register">
+        Sign Up
+      </NavLink>
+    </div>
   );
 }
 
