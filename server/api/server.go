78445db58e5571233f27923b8466e649a5e4d156
Marko Macura <marko.macura9@gmail.com>
2023-08-02 11:47:27 +0200

    Add cors

--- server/api/server.go
+++ server/api/server.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	db "github.com/nemanja731/Event-reminder-web/server/db/sqlc"
 	"github.com/nemanja731/Event-reminder-web/server/token"
@@ -27,6 +28,7 @@ func NewServer(config util.Config, store *db.Store) (*Server, error) {
 		config:     config,
 	}
 	router := gin.Default()
+	router.Use(cors.Default())
 
 	authorized := router.Group("/")
 
@@ -40,6 +42,7 @@ func NewServer(config util.Config, store *db.Store) (*Server, error) {
 		authorized.POST("/add-event", server.addEvent)
 		authorized.DELETE("/delete-event", server.deleteEvent)
 	}
+	authorized.Use(cors.Default())
 
 	server.router = router
 	return server, nil
